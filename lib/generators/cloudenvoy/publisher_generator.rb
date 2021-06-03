76f7e446db093f33a27fa7066d8b528700ff99e3
Don Nguyen <don@spiderbox.design>
2021-06-03 08:07:24 +0700

    Fix rubocop offenses

--- lib/generators/cloudenvoy/publisher_generator.rb
+++ lib/generators/cloudenvoy/publisher_generator.rb
@@ -1,41 +1,41 @@
-require "rails/generators/named_base"
+# frozen_string_literal: true
+
+require 'rails/generators/named_base'
 
 module Cloudenvoy
   module Generators # :nodoc:
     class PublisherGenerator < ::Rails::Generators::NamedBase # :nodoc:
-      desc "This generator creates a CloudEnvoy in app/publishers and a corresponding test"
+      desc 'This generator creates a CloudEnvoy in app/publishers and a corresponding test'
 
-      check_class_collision suffix: "Publisher"
+      check_class_collision suffix: 'Publisher'
 
       def self.default_generator_root
         File.dirname(__FILE__)
       end
 
       def create_publisher_file
-        template "publisher.rb.erb", File.join("app/publishers", class_path, "#{file_name}_publisher.rb")
+        template 'publisher.rb.erb', File.join('app/publishers', class_path, "#{file_name}_publisher.rb")
       end
 
       def create_test_file
         return unless test_framework
 
-        if test_framework == :rspec
-          create_publisher_spec
-        end
+        create_publisher_spec if test_framework == :rspec
       end
 
       private
 
       def create_publisher_spec
         template_file = File.join(
-          "spec/publishers",
+          'spec/publishers',
           class_path,
           "#{file_name}_publisher_spec.rb"
         )
-        template "publisher_spec.rb.erb", template_file
+        template 'publisher_spec.rb.erb', template_file
       end
 
       def file_name
-        @_file_name ||= super.sub(/_?publisher\z/i, "")
+        @file_name ||= super.sub(/_?publisher\z/i, '')
       end
 
       def test_framework
--- lib/generators/cloudenvoy/subscriber_generator.rb
+++ lib/generators/cloudenvoy/subscriber_generator.rb
@@ -1,41 +1,41 @@
-require "rails/generators/named_base"
+# frozen_string_literal: true
+
+require 'rails/generators/named_base'
 
 module Cloudenvoy
   module Generators # :nodoc:
     class SubscriberGenerator < ::Rails::Generators::NamedBase # :nodoc:
-      desc "This generator creates a CloudEnvoy in app/subscribers and a corresponding test"
+      desc 'This generator creates a CloudEnvoy in app/subscribers and a corresponding test'
 
-      check_class_collision suffix: "Subscriber"
+      check_class_collision suffix: 'Subscriber'
 
       def self.default_generator_root
         File.dirname(__FILE__)
       end
 
       def create_subscriber_file
-        template "subscriber.rb.erb", File.join("app/subscribers", class_path, "#{file_name}_subscriber.rb")
+        template 'subscriber.rb.erb', File.join('app/subscribers', class_path, "#{file_name}_subscriber.rb")
       end
 
       def create_test_file
         return unless test_framework
 
-        if test_framework == :rspec
-          create_subscriber_spec
-        end
+        create_subscriber_spec if test_framework == :rspec
       end
 
       private
 
       def create_subscriber_spec
         template_file = File.join(
-          "spec/subscribers",
+          'spec/subscribers',
           class_path,
           "#{file_name}_subscriber_spec.rb"
         )
-        template "subscriber_spec.rb.erb", template_file
+        template 'subscriber_spec.rb.erb', template_file
       end
 
       def file_name
-        @_file_name ||= super.sub(/_?subscriber\z/i, "")
+        @file_name ||= super.sub(/_?subscriber\z/i, '')
       end
 
       def test_framework
